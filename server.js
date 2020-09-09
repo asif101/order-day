cd7acc37e6879143d21b33e390a5868db110c4dc
Asif Rahman <asifrahman8@gmail.com>
2020-09-08 23:04:17 -0400

    added server.js with basic express code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const path = require('path')
+const expressPort = 8080
+// const server = http.createServer(app)
+// const io = require('socket.io').listen(server)
+// const nodemon = require('nodemon')
+
+app.use(express.static('build'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
+// server.listen(3001)
+
+// io.on('connect', socket => {
+    
+// })
+
+app.listen(expressPort, () => {
+    console.log("express server running on port " + expressPort)
+})
