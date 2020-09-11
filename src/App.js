a01a4bbd5e0150afc9b4b00d20e75915edc93191
Asif Rahman <asifrahman8@gmail.com>
2020-09-11 02:19:51 -0400

    added fade in/out animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Intro from './components/Intro/Intro'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import './App.scss'
 
-function App() {
+const App = ({ name }) => {
+
+  const getContent = () => {
+    if (!name) return <Intro />
+    else return <div style={{ width: '100px', height: '100px', backgroundColor: 'red' }}> </div>
+  }
+
   return (
     <div className="App">
-      <Intro />
+      <SwitchTransition mode='out-in'>
+        <CSSTransition key={!name} timeout={300} classNames='CSSTransition-fade'>
+          {getContent()}
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   )
 }
 
-export default App
+const mapStateToProps = ({ order }) => ({ name: order.name })
+
+export default connect(mapStateToProps)(App)
