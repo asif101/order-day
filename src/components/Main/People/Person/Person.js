2eb47789a003dd7d6aec8c5b948bbe5eb93973d5
Asif Rahman <asifrahman8@gmail.com>
2020-09-14 03:16:53 -0400

    added ability to add/remove owners

--- src/components/Main/People/Person/Person.js
+++ src/components/Main/People/Person/Person.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { connect } from 'react-redux'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 import Card from '@material-ui/core/Card'
+import { updateItem } from '../../../../store/actions/orderActions'
 import './Person.scss'
 
-const Person = ({ id, name, items, selectedItemId }) => {
+const Person = ({ id, name, items, selectedItemId, updateItem }) => {
 
     const isOwnerOfSelectedItem = selectedItemId !== null ? items[selectedItemId].owners.includes(id) : false
 
     const handleClick = e => {
         if (selectedItemId !== null) {
-            console.log(selectedItemId)
+            let owners = [...items[selectedItemId].owners]
+            if (isOwnerOfSelectedItem) owners = owners.filter(x => x !== id)
+            else owners.push(id)
+            updateItem({ ...items[selectedItemId], owners })
         }
     }
 
@@ -32,4 +36,4 @@ const mapStateToProps = ({ order, appState }) => ({
     selectedItemId: appState.selectedItemId,
 })
 
-export default connect(mapStateToProps)(Person)
+export default connect(mapStateToProps, { updateItem })(Person)
